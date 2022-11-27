26b09b764e3ab4753019fd8c4c242f0e2a4ec380
eden <goodeden3@gmail.com>
2022-11-27 21:34:39 +0900

    :zap: config domain 6

--- extend/user.config.php
+++ extend/user.config.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가;
 
-if(!isset($_SERVER["HTTPS"])) {
-  header('Location: '. G5_HTTPS_DOMAIN);
-}
+// if(!isset($_SERVER["HTTPS"])) {
+//   header('Location: '. G5_HTTPS_DOMAIN);
+// }
